a83c40b0f4ba434ffcc1352ffc6ca66d0a248aaf
anettfarkas <anettfarkasf@gmail.com>
2017-03-09 17:48:36 -0500

    ex 3-9

---
+++ learn/ex4.1.py
@@ -0,0 +1,7 @@
+views = 1500
+touches = 4500
+touches_per_view = touches / views
+
+print("There are", views, "views.")
+print("Users touched the video", touches, "times.")
+print("That equals", touches_per_view, "touches per view.")
---
+++ learn/ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Anett F. Oswald'
+my_age = 25 #not a lie
+my_height = 6 * 12 + 1 #inches
+my_weight = 145 #lbs
+my_eyes = 'Green'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print("Let's talk about %s." % my_name)
+print("She's %d inches tall." % my_height)
+print("She's %d pounds heavy." % my_weight)
+print("Actually she's not too heavy.")
+print("She's got %s eyes and %s hair." % (my_eyes, my_hair))
+print("Her teeth are usually %s depending on the coffee." % my_teeth)
+
+#this line is tricky, try to get it exactly right
+print ("If I add %d, %d and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight))
